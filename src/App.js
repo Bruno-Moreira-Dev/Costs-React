7c1b6aaac6fdebfb073b40fa6668485c4ce9ca0f
Bruno-Moreira-Dev <marbruge@gmail.com>
2021-10-05 11:47:46 -0300

    feat: criacao da pagina home/form | implementacao backend
    Resolve:[master]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Container from './components/layout/Container/Container';
+import Footer from './components/layout/Footer/Footer';
+import Navbar from './components/layout/Navbar/Navbar';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
 function App() {
   return (
-    <div>
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Container customClass="min-height">
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          
+          <Route path="/company">
+            <Company />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/new-project">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+
+      <Footer />
+    </Router>
   );
 }
 
